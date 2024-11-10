13e4d6fe8846b40098143ad7041e8904f0b584f0
Ba-ray <raybouassy@gmail.com>
2024-11-10 16:17:49 +0200

    Attendance using phone camera

--- backend-js/routes/PhoneCamAttendance.js
+++ backend-js/routes/PhoneCamAttendance.js
@@ -1,9 +1,9 @@
 const express = require('express');
-const multer = require('multer');  // For handling file uploads
+const multer = require('multer');
 const router = express.Router();
 
 const upload = multer({ dest: 'uploads/' });
 
-router.post('/upload', upload.single('image'), uploadImage);
+router.post('/attendance', upload.single('image'), takeAttendance);
 
 module.exports = router;
